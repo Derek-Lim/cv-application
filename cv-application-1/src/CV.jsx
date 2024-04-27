6efe62a40bfae2a9426a7465547dc05509bade80
Derek Lim <dereklim3006@gmail.com>
2024-04-27 18:33:19 -0400

    Add style to CV.

--- cv-application-1/src/CV.jsx
+++ cv-application-1/src/CV.jsx
@@ -10,30 +10,30 @@ export default function CV({ applicantInfo }) {
   return (
     <div className='cv'>
       <div className="general-info">
-        <div>{applicantInfo.name}</div>
-        <div>{applicantInfo.email}</div>
-        <div>{applicantInfo.phoneNumber}</div>
+        <div className="name"><b>{applicantInfo.name}</b></div>
+        <div className="email">{applicantInfo.email}</div>
+        <div className="phone-number">{applicantInfo.phoneNumber}</div>
       </div>
       <div className="education">
         <h2>Education</h2>
         <div className="education-details">
-          <div>{dateConverter(applicantInfo.dateOfStudy)} - present</div>
+          <div className="date-of-study">{dateConverter(applicantInfo.dateOfStudy)} - present</div>
           <div>
-            <div>{applicantInfo.schoolName}</div>
-            <div>{applicantInfo.titleOfStudy}</div>
+            <div className="school-name"><b>{applicantInfo.schoolName}</b></div>
+            <div className="title-of-study">{applicantInfo.titleOfStudy}</div>
           </div>
         </div>
       </div>
       <div className="professional-experience">
         <h2>Professional Experience</h2>
         <div className="professional-experience-details">
-          <div>
+          <div className="work-start-end-date">
             {dateConverter(applicantInfo.workStartDate)} - {dateConverter(applicantInfo.workEndDate)}
           </div>
           <div>
-            <div>{applicantInfo.companyName}</div>
-            <div>{applicantInfo.positionTitle}</div>            
-            <div>{applicantInfo.mainResponsibilites}</div>
+            <div className="company-name"><b>{applicantInfo.companyName}</b></div>
+            <div className="position-title"><i>{applicantInfo.positionTitle}</i></div>            
+            <div className="main-responsibilities">{applicantInfo.mainResponsibilites}</div>
           </div>
         </div>
       </div>
